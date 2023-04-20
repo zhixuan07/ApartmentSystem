d30d237a3e1f958191947539af094f0db87cb456
zhixuan07 <zhixuanchu3131@gmail.com>
2023-04-20 12:07:18 +0800

    add java project

---
+++ Apartment_System/src/main/java/com/mycompany/apartment_system/Apartment_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apartment_system;
+
+/**
+ *
+ * @author zhixu
+ */
+public class Apartment_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
